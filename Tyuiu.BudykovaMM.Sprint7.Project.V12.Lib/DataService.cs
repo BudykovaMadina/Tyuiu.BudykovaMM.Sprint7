cbf186354267fd128910f9104195e1ff527701f2
Мадина <mbudykova@mail.ru>
2025-12-14 20:05:21 +0500

    Add project files.

---
+++ Tyuiu.BudykovaMM.Sprint7.Project.V12.Lib/DataService.cs
@@ -0,0 +1,8 @@
+﻿using System.IO;
+namespace Tyuiu.BudykovaMM.Sprint7.Project.V12.Lib
+{
+    public class DataService
+    {
+
+    }
+}
---
+++ Tyuiu.BudykovaMM.Sprint7.Project.V12.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using Tyuiu.BudykovaMM.Sprint7.Project.V12.Lib;
+namespace Tyuiu.BudykovaMM.Sprint7.Project.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
